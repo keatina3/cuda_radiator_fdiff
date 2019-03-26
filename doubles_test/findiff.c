2656b3ae4250c49685f6705f06844506917d994a
Alex Keating <keatina3@cuda01.tchpc.tcd.ie>
2019-03-26 09:41:36 +0000

    commit

---
+++ doubles_test/findiff.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+#include "findiff.h"
+
+void apply_bounds(double **u, int nx, int ny,
+	double (*lbound)(int, int, int, int),
+	double (*rbound)(int, int, int, int),
+	double (*ubound)(int, int, int, int),
+	double (*bbound)(int, int, int, int))
+	{
+	int i;
+
+	// left & right boundary //
+	for(i=0;i<nx;i++){
+		u[i][0] = lbound(i, 0, nx, ny);
+		u[i][ny-1] = rbound(i, (ny-1), nx, ny);
+	}
+	
+	// upper & bottom boundary //
+	for(i=1;i<(ny-1);i++){
+		u[0][i] = bbound(0,i,nx,ny);
+		u[nx-1][i] = ubound((nx-1),i,nx,ny);
+	}
+}
+
+void iterate(double **uold, double **unew, int p, int nx, int ny){
+	int i,j,k;
+	double **tmp;
+
+	for(k=0;k<p;k++){
+		for(i=0;i<nx;i++){
+			for(j=2;j<ny;j++){
+				unew[i][j] = (1.9*uold[i][(j-2)%ny] + 1.5*uold[i][(j-1)%ny] +
+                    uold[i][j] + 0.5*uold[i][(j+1)%ny] + 0.1*uold[i][(j+2)%ny]);
+				unew[i][j] /= (double)(5.0);
+			}
+		}
+		tmp = uold;
+		uold = unew;
+		unew = tmp;
+	}
+    unew=uold; 
+}
+
+double get_grid_avg(double *u, int nx, int ny){
+	double *row_sum, temp;
+	
+	row_sum = (double*)calloc(nx,sizeof(double));
+	red_rows(u, row_sum, nx, ny);
+	temp = vec_reduce(row_sum, nx);
+	free(row_sum);
+	
+	return temp;
+}
+
+void print_grid(double *u, int nx, int ny, int offset){
+	int i,j;
+	for(i=0;i<nx;i++){
+		for(j=0;j<ny;j++)
+			printf("%f ", u[j+i*offset]);
+		printf("\n");
+	}
+}
---
+++ doubles_test/radiator.c
@@ -0,0 +1,13 @@
+#include "radiator.h"
+
+double fleft(int xind, int yind, int nx, int ny){
+	return 1.0*(double)(1+xind)/(float)(nx);
+}
+
+double fright(int xind, int yind, int nx, int ny){
+	return 0.8*(double)(1+xind)/(float)(nx);
+}
+
+double fzero(int xind, int yind, int nx, int ny){
+	return 0.0;
+}
